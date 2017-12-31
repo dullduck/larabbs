20079d20c4673bbc12f9e6b971e80e1f391a10ed
dullduck <yearningyou@qq.com>
2017-12-31 03:03:14 +0000

    upload topic image

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
