933b957226c5ef6bfc08c1a7e1b88aa36337b99f
vagrant <yearningyou@qq.com>
2018-04-03 11:35:46 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', 'PagesController@root')->name('root');
+///Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 // Auth::routes();
 //
